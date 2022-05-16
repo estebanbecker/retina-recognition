3a5e6ba67b63915473fab7de1a57aa36d6b2471a
Esteban Becker <git@beckeresteban.eu>
2022-05-16 17:27:04 +0200

    Finish

--- retina.py
+++ retina.py
@@ -12,7 +12,7 @@ labels = []
 
 for filename in filename_list:
     img = cv2.imread('RIDB/'+filename)
-
+    img_save = cv2.imread('RIDB/'+filename)
     im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     im_floodfill = im_gray.copy()
     
@@ -27,9 +27,7 @@ for filename in filename_list:
     
     floodfill_mask = cv2.bitwise_not(src=im_floodfill)
     floodfill_mask = cv2.threshold(floodfill_mask, 1, 255, cv2.THRESH_BINARY)[1]
-    cv2.imshow('dil_kernel', floodfill_mask)
     floodfill_mask = cv2.erode(floodfill_mask, dil_kernel, iterations=1)
-    cv2.imshow("im_floodfill", floodfill_mask)
     clahe=cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     im_contrast = clahe.apply(im_gray)
 
@@ -38,14 +36,13 @@ for filename in filename_list:
 
     im_gauss = cv2.GaussianBlur(im_contrast, (7, 7),0)
 
-    im_threshold= cv2.adaptiveThreshold(im_gauss, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11, C=0)
-    cv2.imshow('threshold', im_threshold)
+    im_threshold= cv2.adaptiveThreshold(im_gauss, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, C=0)
+    
     img=cv2.bitwise_and(im_threshold, floodfill_mask)
-    cv2.imshow("winname", img)
-    cv2.waitKey(0)
-    img=cv2.GaussianBlur(img, (5, 5),0)
+
+    img=cv2.medianBlur(img, 5)
     img=cv2.bitwise_not(img)
-    img=cv2.morphologyEx(img, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT,(1,1)))
+    img=cv2.morphologyEx(img, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
     img=cv2.bitwise_not(src=img)
     cv2.imshow("winname", img)
     cv2.waitKey()  
@@ -57,12 +54,16 @@ for filename in filename_list:
     #remove small components (area < 500)
     img2 = np.zeros((output.shape), dtype=np.uint8)
     for i in range(0, nb_components):
-        if sizes[i] >= 500:
+        if stats[i + 1 , cv2.CC_STAT_AREA] >= 500:
             img2[output == i + 1] = 255
 
-    cv2.imshow("winname", img2)
-    cv2.waitKey()    
 
+    cv2.imshow("winname", img2)
+
+    cv2.imshow("original", img_save)
+    cv2.waitKey()
+    img_obj_filtered = img2.copy()
+    
     skel_element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
     skel = np.zeros(img_obj_filtered.shape, np.uint8)
     while True:
@@ -73,7 +74,6 @@ for filename in filename_list:
         img_obj_filtered = eroded.copy()
         if cv2.countNonZero(img_obj_filtered)==0:
             break
-
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
-    labels.append(filename[7])
+    cv2.imshow("winname", skel)
+    cv2.waitKey()
+    
